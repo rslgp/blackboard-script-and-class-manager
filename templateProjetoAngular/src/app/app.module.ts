f75ed7f4cb23344d04b6c04b4ce8823dd725b87d
rslgp <rslgp@cin.ufpe.br>
2019-12-04 12:06:02 -0300

    inserir tela template responder roteiro validado

--- templateProjetoAngular/src/app/app.module.ts
+++ templateProjetoAngular/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { MatTabsModule } from '@angular/material';
+import { MatTabsModule, MatIconModule, MatDividerModule, MatButtonModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HomeComponent } from './components/home/home.component';
+import { ResponderRoteiroComponent } from './components/roteiro/responderRoteiro.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MatTabsModule ],
-  declarations: [ HomeComponent ],
-  bootstrap:    [ HomeComponent ]
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MatTabsModule, MatIconModule, MatDividerModule, MatButtonModule],
+  declarations: [ ResponderRoteiroComponent ],
+  bootstrap:    [ ResponderRoteiroComponent ]
 })
 export class AppModule { }
---
+++ templateProjetoAngular/src/app/components/roteiro/responderRoteiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './responderRoteiro.component.html',
+  //styleUrls: [ './responderRoteiro.component.scss' ]
+})
+export class ResponderRoteiroComponent  {
+  name = 'Angular 6';
+}
