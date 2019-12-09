239d33dc08510d9dcdd5b8516b91e24e8cbbd4a6
Rafael de Souza Leao Gomes Pereira <rslgp@ug3c24.windows.cin.ufpe.br>
2019-12-09 14:53:15 -0300

    tela responder roteiro visual para teste

--- gui/classManager/src/app/roteiro/responder/responder.component.ts
+++ gui/classManager/src/app/roteiro/responder/responder.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './responder.component.html',
   //styleUrls: [ './responderRoteiro.component.scss' ]
 })
-export class ResponderRoteiroComponent  {
-  name = 'Angular 6';
+export class ResponderRoteiroComponent{
+  visibleQuestions = [false,true,false];
+  nextQuestao(event, id){
+    /*
+    //console.log(event.explicitOriginalTarget.attributes[0].ownerElement.parentElement.parentElement.attributes);
+    var idQuestaoText = event.explicitOriginalTarget.attributes[0].ownerElement.parentElement.parentElement.attributes.name.value;
+    //console.log(typeof(idQuestaoText));
+    var questaoId=  idQuestaoText.split("-");
+    var numero = Number(questaoId[1]);
+    console.log(this.visibleQuestions[numero]);
+    */
+    //this.visibleQuestions[numero] = !this.visibleQuestions[numero];
+
+    this.visibleQuestions[id] = !this.visibleQuestions[id];
+
+    //proxima questao
+    this.visibleQuestions[id+1] = !this.visibleQuestions[id+1];
+  }
 }
